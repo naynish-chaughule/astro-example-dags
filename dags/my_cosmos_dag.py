3164a848d21d3c89e71cb7857bac9a55045464f4
naynish-chaughule <naynish.chaughule@kornferry.com>
2024-03-08 13:20:38 -0600

    add dbt

---
+++ dags/my_cosmos_dag.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import os
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+
+profile_config = ProfileConfig(profile_name="default",
+                               target_name="dev",
+                               profile_mapping=PostgresUserPasswordProfileMapping(conn_id="postgres_default", 
+                                                    profile_args={                                                        
+                                                        "schema": "airflow_test"
+                                                        },
+                                                    ))
+
+
+dbt_postgres_dag = DbtDag(project_config=ProjectConfig("/usr/local/airflow/dags/dbt/hello_world",),
+                    operator_args={"install_deps": True},
+                    profile_config=profile_config,
+                    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
+                    schedule_interval="@daily",
+                    start_date=datetime(2023, 9, 10),
+                    catchup=False,
+                    dag_id="dbt_postgres_dag",)
